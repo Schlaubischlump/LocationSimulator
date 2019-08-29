38354f0fcfa8753e7d073d3a2f14b9cd911ac4c1
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2019-08-29 17:15:13 +0200

    Fix Localizable.strings error

--- LocationSimulator/AppDelegate.swift
+++ LocationSimulator/AppDelegate.swift
@@ -12,7 +12,7 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
