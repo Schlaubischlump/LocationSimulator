1174d4f4bf079a7fb94d5e39adbacfb9b97ac44c
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2020-11-03 11:57:45 +0100

    Fix typo

---
+++ LocationSimulator/extensions/NSTableView+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  NSTableView+Extension.swift
+//  LocationSimulator
+//
+//  Created by David Klopp on 03.11.20.
+//  Copyright © 2020 David Klopp. All rights reserved.
+//
+
+// This is just a dummy file to allow compilation of the code
+// on macOS versions lower than 11.0. This defines the new
+// NSTableView style APIs as NOP.
+
+/*import AppKit
+
+@available(mac, introduced: 10.13, unavailable: 10.14)
+public enum NSTableViewStyle: Int {
+    case fullWidth = 1
+}
+
+@available(macOS, introduced: 10.13, unavailable: 10.14)
+extension NSTableView {
+    public var style: NSTableViewStyle {
+        get { return .fullWidth }
+        // swiftlint:disable unused_setter_value
+        set {}
+        // swiftlint:enable unused_setter_value
+    }
+}*/
