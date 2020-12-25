b41656f92b70c7997118d565a5b29e589a9dcd07
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2020-12-25 19:27:34 +0100

    Add missing files

---
+++ LocationSimulator/Preferences/InfoViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ChangeLogViewController.swift
+//  LocationSimulator
+//
+//  Created by David Klopp on 25.12.20.
+//  Copyright © 2020 David Klopp. All rights reserved.
+//
+
+import AppKit
+
+class InfoViewController: NSViewController {
+    override func loadView() {
+        let infoField = NSTextField(frame: .zero)
+        infoField.isEditable = false
+        infoField.isEnabled = false
+        infoField.backgroundColor = .clear
+        infoField.textColor = .labelColor
+        infoField.font = .labelFont(ofSize: NSFont.systemFontSize)
+        infoField.alignment = .center
+        infoField.isBezeled = false
+
+        // Add the welcome text and resize the view
+        infoField.stringValue = "\n" + NSLocalizedString("WELCOME", comment: "") + "\n"
+        infoField.sizeToFit()
+
+        // Add some padding to the left and right
+        infoField.frame.size.width += 40
+
+        self.view = infoField
+    }
+}
