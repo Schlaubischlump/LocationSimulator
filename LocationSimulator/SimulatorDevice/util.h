58f30626a577754892c647695e5a552ccb727f1d
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2021-03-16 15:40:03 +0100

    Inline all util functions and change the error messages

--- LocationSimulator/SimulatorDevice/util.h
+++ LocationSimulator/SimulatorDevice/util.h
@@ -24,14 +24,14 @@ NSString * _Nonnull const kSimBundleID = @"com.apple.iphonesimulator";
 /**
  Load a dynamic library given an absolute path.
  */
-void* _Nullable load_bundle(NSString * _Nonnull path) {
+static inline void* _Nullable load_bundle(NSString * _Nonnull path) {
     if (![[NSFileManager defaultManager] fileExistsAtPath:path]) {
-        NSLog(@"WARNING: Bundle is not present at path: %@", path);
+        NSLog(@"[Error]: Bundle is not present at path: %@", path);
         return nil;
     }
     void* fw = dlopen(path.UTF8String, RTLD_NOW | RTLD_GLOBAL);
     if (!fw) {
-        NSLog(@"ERROR: %s", dlerror());
+        NSLog(@"[Error]: %s", dlerror());
         return nil;
     }
     return fw;
@@ -40,7 +40,7 @@ void* _Nullable load_bundle(NSString * _Nonnull path) {
 /**
  - Return: All possible simulator bridge port names for each Simulator.app instance.
  */
-NSArray<NSNumber *>* _Nonnull getSimulatorPIDs() {
+static inline NSArray<NSNumber *>* _Nonnull getSimulatorPIDs() {
     NSMutableArray<NSNumber *> *ports = [[NSMutableArray alloc] init];
     for (NSRunningApplication *app in NSWorkspace.sharedWorkspace.runningApplications) {
         if ([app.bundleIdentifier isEqualToString: kSimBundleID]) {
@@ -55,7 +55,7 @@ NSArray<NSNumber *>* _Nonnull getSimulatorPIDs() {
  - Parameter pid: the iphonesimulator instance pid
  - Return: portname for the simulator bridge
  */
-NSString * _Nonnull getBridgePortName(pid_t pid) {
+static inline NSString * _Nonnull getBridgePortName(pid_t pid) {
     return [NSString stringWithFormat:@"%@.bridge.%d", kSimBundleID, pid];
 }
 
@@ -66,7 +66,7 @@ NSString * _Nonnull getBridgePortName(pid_t pid) {
  - Parameter portName: the port name of the bridge
  - Return: SimulatorBridge instance
  */
-SimulatorBridge * _Nullable bridgeForSimDevice(SimDevice * _Nonnull device, NSString* _Nonnull portName) {
+static inline SimulatorBridge * _Nullable bridgeForSimDevice(SimDevice * _Nonnull device, NSString* _Nonnull portName) {
     NSError *error = nil;
     mach_port_t bridgePort = [device lookup:portName error:&error];
     if (error == nil && bridgePort != 0) {
@@ -80,9 +80,9 @@ SimulatorBridge * _Nullable bridgeForSimDevice(SimDevice * _Nonnull device, NSSt
 #pragma clang diagnostic pop
             return (SimulatorBridge *) bridgeDistantObject;
         }
-        NSLog(@"Distant Object for port: '%@' is not a SimulatorBridge", portName);
+        NSLog(@"[Error]: Distant Object for port: '%@' is not a SimulatorBridge", portName);
     } else {
-        NSLog(@"Could not get port for name: '%@'", portName);
+        NSLog(@"[Error]: Could not get port for name: '%@'", portName);
     }
     return nil;
 }
@@ -90,7 +90,7 @@ SimulatorBridge * _Nullable bridgeForSimDevice(SimDevice * _Nonnull device, NSSt
 /**
  - Return: path to the currently active developer dir
  */
-NSString * _Nullable getActiveDeveloperDir() {
+static inline NSString * _Nullable getActiveDeveloperDir() {
     NSString *xcodeSelectPath = @"/usr/bin/xcode-select";
 
     // If xcode-select is not installed, just skip the simulator support.
@@ -116,7 +116,7 @@ NSString * _Nullable getActiveDeveloperDir() {
     if (task.terminationStatus == 0) {
         grepOutput = [[NSString alloc] initWithData:[file readDataToEndOfFile] encoding:NSUTF8StringEncoding];
     } else {
-        NSLog(@"%@ failed with error code: %d", xcodeSelectPath, task.terminationStatus);
+        NSLog(@"[Error]: %@ failed with error code: %d", xcodeSelectPath, task.terminationStatus);
     }
     [file closeFile];
 
