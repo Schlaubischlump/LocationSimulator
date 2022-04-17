03a9ab6a90259f4d925e673b200a5b618c5cce42
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2022-04-17 14:58:35 +0200

    Cleanup logging folder structure

--- LocationSimulator/Extensions/Foundation/FileManager+Logger.swift
+++ LocationSimulator/Extensions/Foundation/FileManager+Logger.swift
@@ -56,7 +56,7 @@ let kLogFileName = "log.txt"
         // Init the console logger
         logger_autoFlush(5000) // Flush every 5 seconds
         logger_initConsoleLogger(nil)
-        logger_setLevel(LogLevel_DEBUG)
+        logger_setLevel(LogLevel_INFO)
 
         // Init the file logger
         let logPath = FileManager.default.logFile.path
